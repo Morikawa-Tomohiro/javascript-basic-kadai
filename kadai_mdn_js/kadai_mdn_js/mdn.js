1fdd86d5cb9027746d6a73587adbfd25dd16066b
Morikawa-Tomohiro <151585648+Morikawa-Tomohiro@users.noreply.github.com>
2023-11-25 15:38:13 +0900

    Add files via upload

---
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const date = new Date();
+
+const [month, day, year] = [
+  date.getMonth(),
+  date.getDate(),
+  date.getFullYear(),
+];
+
+console.log( year + '年' + month + '月' + day + '日');
