b6c80307b83d580ede1366174752b2bce9d58b1a
Morikawa-Tomohiro <151585648+Morikawa-Tomohiro@users.noreply.github.com>
2023-11-25 15:00:58 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,7 +3,7 @@
 let today = new Date();
 
 let year = today.getFullYear();
-let month = today.getMonth() - 2;
-let day = today.getDate() - 3;
+let month = today.getMonth() ;
+let day = today.getDate() ;
 
 console.log( year + '年' + month + '月' + day + '日');
